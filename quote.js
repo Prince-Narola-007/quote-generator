0b0857a7280f3cbaa35488746c7f959f091c2db8
Prince-Narola007 <narolaparice@gmail.com>
2024-02-12 11:47:53 +0530

    first

---
+++ quote.js
@@ -0,0 +1,29 @@
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+
+
+const getNewQuote = async () => {
+
+    var url = "https://type.fit/api/quotes";
+
+
+    const response = await fetch(url);
+
+    const allQuotes = await response.json();
+
+    const ind = Math.floor(Math.random() * allQuotes.length);
+
+    const quote = allQuotes[ind].text;
+
+    const auth = allQuotes[ind].author;
+
+    if (auth == null) {
+        author = "Anonymous";
+    }
+
+
+    text.innerHTML = quote;
+    author.innerHTML = auth;
+
+}
+getNewQuote();
